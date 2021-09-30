37da0b51bb974a950bf7748f45cee9bf1f8ef861
kazun1224 <75646380 +kazun1224@users.noreply.github.com>
2021-09-30 13:05:56 +0900

    Create MemoEditScreen and MemoCreateScreen

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createIconSetFromIcoMoon } from "@expo/vector-icons";
-import { useFonts } from '@use-expo/font';
+import { useFonts } from '@use-expo/font'; // .ttfファイルを使用できる機能をimport
 import { number, string, oneOf } from "prop-types"; // oneOfは決められた値を自由に割り付けることができる
 
 import icomoon from '../../assets/fonts/icomoon.ttf';
@@ -19,12 +19,14 @@ export default function Icon(props) {
   return <CustomIcon name={name} size={size} color={color} />;
 }
 
+// propTypesを設定
 Icon.protoTypes = {
   name: oneOf(['plus', 'pen', 'check', 'x']).isRequired, // oneOfの値を決める
   size: number,
   color: string,
 }
 
+// defaultPropsを設定
 Icon.defaultProps = {
     size: 24,
     color: '#000000',
---
+++ src/screens/MemoEditScreen.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, StyleSheet, TextInput, KeyboardAvoidingView} from 'react-native';
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+import Icon from '../components/Icon';
+
+export default function MemoEditScreen() {
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <AppBar />
+      <View style={styles.inputContainer}>
+          <TextInput value='買い物リスト' multiline style={styles.input} />
+      </View>
+      <CircleButton name='check' />
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8',
+  },
+  inputContainer: {
+    paddingVertical: 32, // 縦幅
+    paddingHorizontal: 27, //横幅
+    flex: 1, //画面いっぱいに表示
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  }
+});
