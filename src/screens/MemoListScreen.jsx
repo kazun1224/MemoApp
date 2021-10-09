33ac952da414d11e4a6649582739057033484638
kazun1224 <75646380 +kazun1224@users.noreply.github.com>
2021-10-09 12:44:28 +0900

    Create Component Loading

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,14 +1,17 @@
 import React, {useEffect, useState} from "react";
-import { StyleSheet, View, Alert } from 'react-native';
+import { StyleSheet, View, Text, Alert } from 'react-native';
 import firebase from "firebase";
 
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
 import LogoutButton from "../components/LogoutButton";
+import Button from "../components/Button";
+import Loading from "../components/Loading";
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
   const [memos, setMemos] = useState([]);
+  const [isLoading,setLoading] = useState(false);
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => <LogoutButton />,
@@ -20,6 +23,7 @@ export default function MemoListScreen(props) {
     const { currentUser } = firebase.auth();
     let unsubscribe = () => {};
     if(currentUser) {
+      setLoading(true);
       const ref = db.collection(`user/${currentUser.uid}/memos`).orderBy('updatedAT','desc');
       unsubscribe = ref.onSnapshot((snapshot) => {
         //カラの配列を準備
@@ -37,14 +41,28 @@ export default function MemoListScreen(props) {
         });
         //配列に入ったデータをuseStateでsetMemosにセットする
         setMemos(userMemos);
+        setLoading(false);
       },(error) => {
         console.log(error);
+        setLoading(false);
         Alert.alert('データの読み込みに失敗しました。');
       });
     }
     //監視し続けるのでキャンセルするためにunsubscribeで定義して最後にunsubscribeを返す
     return unsubscribe;
   },[]);
+
+  if(memos.length === 0) {
+    return (
+      <View style={emptyStyles.container}>
+        <Loading isLoading={isLoading} />
+        <View style={emptyStyles.inner}>
+          <Text style={emptyStyles.title}>最初のメモを作成しよう！</Text>
+          <Button style={emptyStyles.button} label="作成する" onPress={() => { navigation.navigate('MemoCreate');}}></Button>
+        </View>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
@@ -62,3 +80,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0F4F8',
   },
 });
+
+const emptyStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  inner: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 24,
+  },
+  button: {
+    alignSelf: 'center',
+  },
+})
